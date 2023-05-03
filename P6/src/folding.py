42c2af5e868b0718a49f82ce3076ecdadcbaf9a2
Genona Torruella <genona.torruella@gmail.com>
2023-05-03 18:25:24 +0200

    p6

--- P6/src/folding.py
+++ P6/src/folding.py
@@ -1,80 +1,114 @@
 import random
 import numpy as np
 
-def initialize_lattice(size):
-    """
-    Initializes a 2D lattice of a given size with all elements set to 0.
-    """
-    lattice = np.zeros((size, size), dtype=int)
-    return lattice
+
+def initialize_protein_old(hp_string):
+    left = {}
+    right = {}
+    mid = len(hp_string) // 2
+    
+    for pos, char in enumerate(hp_string):
+        mod_pos = pos % 9
+        if char == 'h':
+            if mod_pos % 2 == 0 and pos < mid:
+                left[mod_pos] = (pos, char)
+            elif mod_pos % 2 == 1 and pos < mid:
+                left[mod_pos] = (pos, char)
+            elif mod_pos % 2 == 0 and pos >= mid:
+                right[mod_pos] = (pos, char)
+            elif mod_pos % 2 == 1 and pos >= mid:
+                right[mod_pos] = (pos, char)
+    
+    print(left)
+    print('-----------------')
+    print(right)
+    max_size = 0
+    max_matching = ()
+    
+    for pos, left_tuple in left.items():
+        if pos in right:
+            right_tuple = right[pos]
+            if left_tuple[1] == right_tuple[1]:
+                matching_size = abs(left_tuple[0] - right_tuple[0]) + 1
+                if matching_size > max_size:
+                    max_size = matching_size
+                    max_matching = (left_tuple[1], left_tuple[0], right_tuple[1], right_tuple[0])
+    
+    return max_matching
 
 def initialize_protein(hp_string):
-    """
-    Initializes a protein chain from an HP string, placing the first amino acid
-    at the center of the lattice and the remaining amino acids in a diagonal line.
-    """
-    size = len(hp_string) * 2 + 1
-    lattice = initialize_lattice(size)
-    x = size // 2
-    y = size // 2
-    for amino_acid in hp_string:
-        if amino_acid == "H":
-            lattice[x][y] = 1
-        x += 1
-        y += 1
-    return lattice
+    odd = []
+    even = []
+    for pos, char in enumerate(hp_string):
+        is_odd = pos % 2
+        if char == 'h':
+            if is_odd == 0:
+                even.append(pos)
+            else:
+                odd.append(pos)
+    
+    print(even,odd)
+    min_size = np.inf
+    matching = {}
+    i,j = 0,0
+    for even,odd in zip(even,reversed(odd)):
+        if even < odd:
+            matching[even] = odd
+        else:
+            matching[odd] = even
+        if abs(even-odd) < min_size:
+            min_size = abs(even-odd)
+            i,j = even,odd
+    
+    p = (i+j) // 2
+    
+    return matching,p
 
-def score_protein(protein):
+def score(protein):
     """
     Computes the energy of a folded protein using the HP model scoring function.
     """
-    energy = 0
-    size = protein.shape[0]
-    for x in range(size):
-        for y in range(size):
-            if protein[x][y] == 1:
-                if x > 0 and protein[x-1][y] == 1:
-                    energy -= 1
-                if y > 0 and protein[x][y-1] == 1:
-                    energy -= 1
-                if x < size-1 and protein[x+1][y] == 1:
-                    energy -= 1
-                if y < size-1 and protein[x][y+1] == 1:
-                    energy -= 1
-    return energy
+    return 0
 
-def fold_protein(protein, iterations):
+
+def fold_protein(d,p,n):
+    
     """
     Implements the 1/4 approximation algorithm for folding a protein chain in a given
     number of iterations.
     """
-    size = protein.shape[0]
-    for i in range(iterations):
-        x = random.randint(1, size - 2)
-        y = random.randint(1, size - 2)
-        amino_acid = protein[x][y]
-        neighbor_sum = (protein[x-1][y] + protein[x+1][y] +
-                        protein[x][y-1] + protein[x][y+1])
-        if amino_acid == 0 and neighbor_sum == 1:
-            protein[x][y] = 1
-        elif amino_acid == 1 and neighbor_sum in (0, 2):
-            protein[x][y] = 0
-    return protein
-def visualize_protein(protein):
+    diff = 0
+    res = "f"*(n-1)
+    print(d)
+    for key in d:
+        diff = key - diff
+        print(diff, key)
+        if diff > 3:
+            ## construct the little house
+            t=(diff//2)-1
+            casa = "l" + "f"*t + 2*"r" + "f"*t + "l"
+            npos = key + len(casa)-1
+            res= res[:key] + casa + res[npos:]
+            print(res)
+    print(p)
+    res = res[:p] + 2 * "r" + res[p+2:]
+    return res
+
+
+def visualize(protein):
     """
     Generates an ASCII art representation of a folded protein chain.
     """
-    protein_str = ""
-    for row in protein:
-        protein_str += "".join(["H" if val == 1 else "." for val in row])
-        protein_str += "\n"
-    return protein_str
+    return ''
 
-# Example usage:
-hp_string = "HPHPPHHPHPPH"
-protein = initialize_protein(hp_string)
-folded_protein = fold_protein(protein, 10000)
-energy = score_protein(folded_protein)
-print("Folded protein:")
-print(visualize_protein(folded_protein))
-print(f"Energy: {energy}")
+
+def main():
+    # Example usage:
+    hp_string = "hphpphpphhphh"
+    print(hp_string)
+    matching, p = initialize_protein(hp_string.lower())
+    folded = fold_protein(matching, p, len(hp_string))
+    print(folded)
+     
+if __name__ == "__main__":
+    main()
